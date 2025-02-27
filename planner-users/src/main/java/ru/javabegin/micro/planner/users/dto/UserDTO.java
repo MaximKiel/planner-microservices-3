8d6ab2cf4290f0a3c4364124497d16bcf9daa9eb
MaximKiel <loup.du.nord@yandex.ru>
2025-02-27 16:23:26 +0300

    Change type UserDTO.id to String

--- planner-users/src/main/java/ru/javabegin/micro/planner/users/dto/UserDTO.java
+++ planner-users/src/main/java/ru/javabegin/micro/planner/users/dto/UserDTO.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @NoArgsConstructor
 public class UserDTO {
 
-    private Long id;
+    private String id;
     private String email;
     private String username;
     private String password;
